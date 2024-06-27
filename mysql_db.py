7f8dbffbf86d3b808160d0163995454025b1d1be
Ebrahim_ak <y560mia3@gmail.com>
2024-06-27 22:33:49 +0330

    sql module added

---
+++ mysql_db.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+if __name__ != '__main__':
+    class Database:
+        def __init__(self, user, password, host):
+            self.mydb = mysql.connector.connect(user=user, password=password, host=host)
+            self.curser = self.mydb.cursor()
+
+        def create_database(self, database_name):
+            self.curser.execute(f'CREATE DATABASE IF NOT EXISTS {database_name}')
+            self.curser.execute(f'USE {database_name}')
+
+        def create_table(self,table_one,table_two):
+            self.curser.execute(f'CREATE TABLE IF NOT EXISTS {table_one}(ID INT AUTO_INCREMENT PRIMARY KEY,NAME VARCHAR(255) UNIQUE NOT NULL,PRICE INT NOT NULL,IS_FOOD BOOLEAN DEFAULT TRUE)')
+            self.curser.execute(f'CREATE TABLE IF NOT EXISTS {table_two}(ID INT AUTO_INCREMENT PRIMARY KEY,RECEIPT_ID INT,MENU_ID INT,COUNT INT,FOREIGN KEY(MENU_ID) REFERENCES {table_one}(ID))')
+
+        def insert_data(self, table_name, data):
+            sql = f'INSERT INTO {table_name}({", ".join(data.keys())}) VALUES({", ".join(["%s" for _ in range(len(data))])})'
+            self.curser.executemany(sql, [tuple(data.values())])
+
+        def commit(self):
+            self.mydb.commit()
+
+        def close(self):
+            self.curser.close()
+            self.mydb.close()
